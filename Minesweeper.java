add371e82cef70321d211eacc61daf9e37cd3261
Hcech64 <Hcechini@lshigh.org>
2023-05-30 17:21:03 -0700

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -3,20 +3,16 @@ import java.util.*;
 public class Minesweeper{
 
     //choose a box to click and see the reaction
-    public static String[][] click(String[][] visible, int[][] hidden, int size) {
-        Scanner keyboard = new Scanner(System.in); 
-        System.out.println("Where would you like to go? Eg. 4, 5");
-        String stringCords = keyboard.nextLine(); // ERROR HERE
-        String value = stringCords.replaceAll("[^0-9]", "");  // POSSIBLY HERE
-        String stringX = value.substring(0, 1);
-        String stringY = value.substring(value.length() - 2, value.length() - 1);
-        int X = Integer.parseInt(stringX);
-        int Y = Integer.parseInt(stringY);
+    public static String[][] click(int X, int Y, String[][] visible, int[][] hidden, int size) {
+        
+        visible[X][Y] = Integer.toString(hidden[X][Y]);
 
-        visible[X][Y] = "_";
+        if(visible[X][Y].equals("0")){
+            //click(visible, hidden, size)
+        }
         visible = revealAround(size, X, Y, visible, hidden);
         PrintBoard(visible);
-        keyboard.close();
+        
         return visible;
     }
 
@@ -67,17 +63,34 @@ public class Minesweeper{
                 visible[x][y] = "X";
             }
         }
-        PrintBoard(visible);
+        //PrintBoard(visible);
         
 
         int[][] hidden = new int[size][size];
         bombPlacer(hidden, size, difficulty);
+
+        PrintBoard(hidden);
+
+        hidden = BombAdder(hidden);
         PrintBoard(hidden);
 
         //game loop
         Boolean win = false;
         while(win == false){
-            PrintBoard(click(visible, hidden, size));
+
+            Scanner keyboard = new Scanner(System.in); 
+            System.out.println("Where would you like to go? Eg. 4, 5");
+            String stringCords = keyboard.nextLine(); // ERROR HERE
+            String value = stringCords.replaceAll("[^0-9]", "");  // POSSIBLY HERE
+            String stringX = value.substring(0, 1);
+            String stringY = value.substring(value.length() - 2, value.length() - 1);
+            int X = Integer.parseInt(stringX);
+            int Y = Integer.parseInt(stringY);
+
+
+            // NEED TO FIX RECURSION
+            PrintBoard(click(X, Y, visible, hidden, size));
+            keyboard.close();
         }
         
         k.close();
@@ -104,79 +117,72 @@ public class Minesweeper{
         
         int[][]hidden2 = hidden;
         for (int column = 0; column <= hidden.length -1; column++) {
-
             for(int row = 0; row <= hidden[column].length -1; row++){
-                if(column != 0){
-                    hidden2[column -1][row] = hidden2[column][row] += hidden[column-1][row];
-                    if(row != 0){
-                        if(hidden[column ][row-1] != -1){
-                            hidden2[column ][row-1] = Integer.toString(hidden[x][y-1]);
-                        }
-                        if(hidden[column-1 ][row+1]!= -1){
-                            hidden2[column-1 ][row-1] = Integer.toString(hidden[x][y-1]);
-                        }
-                    } 
-                    if(row != hidden[column].length){
-                        if(hidden[column ][row+1]!= -1){
-                            hidden2[column ][row+1] = Integer.toString(hidden[x][y+1]);
-                        }
-                        if(hidden[column][row+1]!= -1){
-                            hidden2[column-1 ][row+1] = Integer.toString(hidden[x][y+1]);
+
+                int bomb = 0;
+                int[][] mini = AroundTile(hidden, column, row);
+                    for (int i = 0; i <= mini.length -1; i++) {
+                        for(int j = 0; j <= mini[i].length -1; j++){
+                            if(mini[i][j] == -1){
+                              bomb++;  
+                            }
+                            
                         }
                     }
-                }
-                else if(column != hidden[column].length){
-                    hidden2[column -1][row] = Integer.toString(hidden[x-1][y]);
-                    if(row != 0){
-                        if(hidden[column ][row-1]!= -1){
-                            hidden2[column ][row-1] = Integer.toString(hidden[x][y-1]);
-                        }
-                        if(hidden[column+1 ][row-1]!= -1){
-                            hidden2[column+1 ][row-1] = Integer.toString(hidden[x][y-1]);
-                        }
-                    } 
-                    if(row != hidden[column].length){
-                        if(hidden[column ][row-1]!= -1){
-                            hidden2[column ][row-1] = Integer.toString(hidden[x][y+1]);
-                        }
-                        if(hidden[column+1 ][row+1]!= -1){
-                            hidden2[column+1 ][row+1] = Integer.toString(hidden[x][y+1]);
-                        }
-                    }
-                }
-                if(row != 0){
-                    hidden2[column -1][row] = Integer.toString(hidden[x-1][y]);
-                    if(column != 0){
-                        hidden2[column-1 ][row] = Integer.toString(hidden[x][y-1]);
-                        hidden2[column-1 ][row-1] = Integer.toString(hidden[x][y-1]);
-                    } 
-                    if(column != hidden[column].length){
-                        hidden2[column+1 ][row] = Integer.toString(hidden[x][y+1]);
-                        hidden2[column+1 ][row-1] = Integer.toString(hidden[x][y+1]);
-                    }
-                }
-                if(row != hidden[column].length){
-                    hidden2[column -1][row] = Integer.toString(hidden[x-1][y]);
-                    if(column != 0){
-                        hidden2[column-1 ][row] = Integer.toString(hidden[x][y-1]);
-                        hidden2[column-1 ][column+1] = Integer.toString(hidden[x][y-1]);
-                    } 
-                    if(column != hidden[column].length){
-                        hidden2[column+1 ][row] = Integer.toString(hidden[x][y+1]);
-                        hidden2[column+1 ][row+1] = Integer.toString(hidden[x][y+1]);
-                    }
-                }
+                if(hidden2[column][row] != -1){
+                   hidden2[column][row] += bomb; 
+                }            
             }
         }
 
         return hidden2;
     }
 
+    //create an array of all the spaces around a coord
+    public static int[][] AroundTile(int[][] hidden, int x, int y){
+
+        int size = hidden.length -1;
+
+        int[][] mini = new int[3][3];
+        mini[1][1] = hidden[x][y];
+
+        if(x != 0 && y!= 0){
+            mini[0][0] = hidden[x-1][y-1];
+        }
+        if(x!=0){
+            mini[0][1] = hidden[x-1][y];
+        }
+        if(x!=0 && y != size){
+            mini[0][2] = hidden[x-1][y+1];
+        }
+        if(y != 0){
+            mini[1][0] = hidden[x][y-1];
+        }
+        if(y != size){
+            mini[2][2] = hidden[x][y+1];
+        }
+        if(x!= size && y !=0){
+            mini[2][0] = hidden[x+1][y-1];
+        }
+        if(x!= size){
+            mini[2][1] = hidden[x+1][y];
+        }
+        if(x != size && y != size){
+            mini[2][2] = hidden[x+1][y+1];
+        }
+        return mini;
+    }
+
     //print out the current board
     public static void PrintBoard(int[][] grid){
         for(int x = 0; x < grid.length; x++){
             for(int y = 0; y < grid[0].length; y++){
-                System.out.print( grid[x][y] + "  ");
+                if(grid[x][y] == -1){
+                    System.out.print( grid[x][y] + "  ");
+                }
+                else{
+                    System.out.print( " " + grid[x][y] + "  ");
+                }
             }
             System.out.println();
         }
@@ -185,7 +191,12 @@ public class Minesweeper{
     public static void PrintBoard(String[][] grid){
         for(int x = 0; x < grid.length; x++){
             for(int y = 0; y < grid[0].length; y++){
-                System.out.print( grid[x][y] + "  ");
+                if(grid[x][y].equals("-1")){
+                    System.out.print( grid[x][y] + "  ");
+                }
+                else{
+                    System.out.print( " "+ grid[x][y] + "  ");
+                }
             }
             System.out.println();
         }
