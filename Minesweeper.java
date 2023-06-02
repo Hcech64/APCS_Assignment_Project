321821b8ef38a987077f5287fcdf08151e9c7b18
Hcech64 <Hcechini@lshigh.org>
2023-06-01 22:57:25 -0700

    Done

--- Minesweeper.java
+++ Minesweeper.java
@@ -3,15 +3,11 @@ import java.util.*;
 public class Minesweeper {
 
     public static void main(String[] args) {
-        // Welome to this AP Computer science assignment. In this assignment, you will
-        // use you kowledge of arrays and 2d arrays in order to constuct a minesweeper
-        // game
-        // import keyboard handler
+    
+        // Ask user for Board size and game difficulty
 
         Scanner k = new Scanner(System.in);
-
         int size = 0;
-
         while (size == 0) {
             System.out.println("What size field do you want? Small, Medium, or Large?\n");
             String temp = k.nextLine();
@@ -26,7 +22,7 @@ public class Minesweeper {
         }
 
         double difficulty = 0.0;
-        boolean hackSwitch = false;
+        boolean hackSwitch = false; // reveals board for dbug perposes (or winning)
 
         while (difficulty == 0.0) {
             System.out.println("what difficulty do you want? easy, medium, or hard?");
@@ -47,42 +43,55 @@ public class Minesweeper {
             }
         }
 
+        //Initialize and se up the boards
         String[][] visible = new String[size][size];
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
-                visible[x][y] = "\u001B[31m" + "X" + "\u001B[0m";
+                visible[x][y] = "\u001B[31m" + "X" + "\u001B[0m"; //set all elements of the visible array to "X"
             }
         }
-        // PrintBoard(visible);
-
-        Scanner keyboard = new Scanner(System.in);
         int[][] hidden = new int[size][size];
         bombPlacer(hidden, size, difficulty);
-
         hidden = BombAdder(hidden);
 
-        PrintBoard(visible);
+        // count the total bombs
+        int bomb = 0;
+        for (int[] i : hidden) {
+            for (int j : i) {
+                if(j == -1){bomb++;}
+            } 
+        }
 
-        // game loop
+        PrintBoard(visible);//prints the board to start
+
+        // game loop - runs until win/loose
         int win = 0;
         while (win == 0) {
-            if (hackSwitch == true) {
-                PrintBoard(hidden);
-            }
+            if (hackSwitch == true) {PrintBoard(hidden);} // prints hidden for dbug
+
+
             System.out.println("Where would you like to go? Eg. 4 5");
-            String stringCords = keyboard.nextLine();
+            String stringCords = k.nextLine();
 
             int X = Integer.parseInt(stringCords.substring(0, 1));
             int Y = Integer.parseInt(stringCords.substring(stringCords.length() - 1));
 
             System.out.println("Mining hole at: " + X + ", " + Y);
 
-            visible = click(X, Y, visible, hidden, size);
+            visible[X][Y] = Integer.toString(hidden[X][Y]);
             PrintBoard(visible);
 
-            if (hidden[X][Y] == -1) {
-                win = -1;
+            //check for loose
+            if (hidden[X][Y] == -1) {win = -1;}
+
+            // check for win
+            int flags =0;
+            for (String[] i : visible) {
+                for (String j : i) {
+                    if(j.equals("\u001B[31m" + "X" + "\u001B[0m")){flags++;}
+                } 
             }
+            if(flags == bomb){win = 1;};
         }
 
         if (win == 1) {
@@ -91,11 +100,10 @@ public class Minesweeper {
         if (win == -1) {
             System.out.println("You clicked a bomb.You loose");
         }
-
-        keyboard.close();
         k.close();
     }
 
+    // randomly places bombs on the baord
     public static void bombPlacer(int[][] grid, int size, double difficulty) {
 
         // set all to zero
@@ -105,6 +113,7 @@ public class Minesweeper {
             }
         }
 
+        // places the bombs at random coords
         int bombCounter = 0;
         bombCounter = (int) (size * size * difficulty);
 
@@ -113,6 +122,7 @@ public class Minesweeper {
         }
     }
 
+    // adds the number of bombs surroundig a coordinate
     public static int[][] BombAdder(int[][] hidden) {
 
         int[][] hidden2 = hidden;
@@ -126,7 +136,6 @@ public class Minesweeper {
                         if (mini[i][j] == -1) {
                             bomb++;
                         }
-
                     }
                 }
                 if (hidden2[column][row] != -1) {
@@ -134,11 +143,10 @@ public class Minesweeper {
                 }
             }
         }
-
         return hidden2;
     }
 
-    // create an small array of all the spaces around a coord
+    // create an small 3x3 array of all the spaces around a coord
     public static int[][] AroundTile(int[][] hidden, int x, int y) {
 
         int size = hidden.length - 1;
@@ -146,6 +154,8 @@ public class Minesweeper {
         int[][] mini = new int[3][3];
         mini[1][1] = hidden[x][y];
 
+
+        // make sure that code doesnt call outside of the array. Checks for edges and corners
         if (x != 0 && y != 0) {
             mini[0][0] = hidden[x - 1][y - 1];
         }
@@ -173,7 +183,7 @@ public class Minesweeper {
         return mini;
     }
 
-    // print out the current board
+    // print out the current board - for hidden which is an Int[][]
     public static void PrintBoard(int[][] grid) {
         for (int x = 0; x < grid.length; x++) {
             for (int y = 0; y < grid[0].length; y++) {
@@ -187,7 +197,7 @@ public class Minesweeper {
         }
         System.out.println("\n \n");
     }
-
+    // print out the current board - for visible which is an String[][]
     public static void PrintBoard(String[][] grid) {
         for (int x = 0; x < grid.length; x++) {
             for (int y = 0; y < grid[0].length; y++) {
@@ -200,41 +210,5 @@ public class Minesweeper {
             System.out.println();
         }
         System.out.println("\n \n");
-    }
-
-    // choose a box to click and see the reaction
-    public static String[][] click(int x, int y, String[][] visible, int[][] hidden, int size) {
-
-        visible[x][y] = Integer.toString(hidden[x][y]);
-        /*
-         * if(hidden[x][y] == 0){
-         * if(x != 0 && y!= 0){
-         * click(x-1, y-1, visible, hidden, size);
-         * }
-         * if(x!=0){
-         * click(x-1, y, visible, hidden, size);
-         * }
-         * if(x!=0 && y != size){
-         * click(x-1, y+1, visible, hidden, size);
-         * }
-         * if(y != 0){
-         * click(x, y-1, visible, hidden, size);
-         * }
-         * if(y != size){
-         * click(x, y+1, visible, hidden, size);
-         * }
-         * if(x!= size && y !=0){
-         * click(x+1, y-1, visible, hidden, size);
-         * }
-         * if(x!= size){
-         * click(x+1, y, visible, hidden, size);
-         * }
-         * if(x != size && y != size){
-         * click(x+1, y+1, visible, hidden, size);
-         * }
-         * }
-         */
-
-        return visible;
     }
 }
